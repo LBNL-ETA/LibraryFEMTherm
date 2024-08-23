7d662a675ccd92b271b0f9f8bd9042d572a40429
vidanovic <DVVidanovic@lbl.gov>
2024-08-23 09:40:32 -0700

    Project is now using Fecthcontent_Declare. It also disables warning messages from xmlParser.

--- cmake/LibraryFEMTHERMCompilerFlags.cmake
+++ cmake/LibraryFEMTHERMCompilerFlags.cmake
@@ -12,9 +12,9 @@ IF ( CMAKE_COMPILER_IS_GNUCXX OR "x${CMAKE_CXX_COMPILER_ID}" STREQUAL "xClang" )
     ADD_CXX_DEFINITIONS("-pipe") # Faster compiler processing
     # set (CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++")
     if( MINGW )
-      ADD_CXX_DEFINITIONS("-std=gnu++14") # Enable C++11 features in MINGW
+      ADD_CXX_DEFINITIONS("-std=gnu++17") # Enable C++11 features in MINGW
     else()
-      ADD_CXX_DEFINITIONS("-std=c++14") # Enable C++11 features in g++
+      ADD_CXX_DEFINITIONS("-std=c++17") # Enable C++11 features in g++
       if(NOT WIN32)
         ADD_CXX_DEFINITIONS("-fPIC")
       endif()
